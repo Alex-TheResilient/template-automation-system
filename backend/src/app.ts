de205f47276b74f7343c58f5711d7ae05129338c
Alex Huaracha <alex.ronny.hq@gmail.com>
2024-12-09 08:22:00 -0500

    fix: router

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'; 
-import { initSystem } from './src/initialization';
-import authRoutes from './src/routes/authRoutes';
-import organizacionRoutes from './src/routes/organizacion.routes';
-import proyectoRoutes from './src/routes/proyecto.routes';
+import { initSystem } from './initialization';
+import authRoutes from './routes/authRoutes';
+import organizacionRoutes from './routes/organizacion.routes';
+import proyectoRoutes from './routes/proyecto.routes';
 
 dotenv.config(); // Cargar las variables de entorno desde el archivo .env
 
