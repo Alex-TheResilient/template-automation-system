77f76e6aed59f055a0f11d3e57129a8a6e524b29
EmersonQH <equispehuaman@unsa.edu.pe>
2025-05-20 01:46:17 -0500

    Fix: Allow duplicate expert and source codes across all projects by adding a composite unique constraint (code, project ID)

--- backend/prisma/migrations/20250517063620_allow_duplicate_expert_codes_across_projects/migration.sql
+++ backend/prisma/migrations/20250517063620_allow_duplicate_expert_codes_across_projects/migration.sql
@@ -1,2 +1,5 @@
 -- DropIndex
 DROP INDEX "Expert_code_key";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Expert_code_projectId_key" ON "Expert"("code", "projectId");
--- backend/prisma/migrations/20250518044745_source_code_unique_per_project/migration.sql
+++ backend/prisma/migrations/20250518044745_source_code_unique_per_project/migration.sql
@@ -1,2 +1,5 @@
 -- DropIndex
 DROP INDEX "Source_code_key";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Source_code_projectId_key" ON "Source"("code", "projectId");
