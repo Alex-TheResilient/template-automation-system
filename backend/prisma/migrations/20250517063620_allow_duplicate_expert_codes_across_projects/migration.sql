ea33e2a17dce661c840899cbc36322dc2dc42294
EmersonQH <equispehuaman@unsa.edu.pe>
2025-05-20 16:07:31 -0500

    Some errors with prism are corrected

--- backend/prisma/migrations/20250517063620_allow_duplicate_expert_codes_across_projects/migration.sql
+++ backend/prisma/migrations/20250517063620_allow_duplicate_expert_codes_across_projects/migration.sql
@@ -1,5 +1,3 @@
 -- DropIndex
 DROP INDEX "Expert_code_key";
 
--- CreateIndex
-CREATE UNIQUE INDEX "Expert_code_projectId_key" ON "Expert"("code", "projectId");
--- backend/prisma/migrations/20250518044745_source_code_unique_per_project/migration.sql
+++ backend/prisma/migrations/20250518044745_source_code_unique_per_project/migration.sql
@@ -1,5 +1,3 @@
 -- DropIndex
 DROP INDEX "Source_code_key";
 
--- CreateIndex
-CREATE UNIQUE INDEX "Source_code_projectId_key" ON "Source"("code", "projectId");
